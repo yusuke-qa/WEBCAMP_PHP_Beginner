6c91e95429925643110cabd9723a212fc0b2eb35
yusuke <shindo1320366@gmail.com>
2026-04-02 12:14:55 +0000

    define.phpを書いた

---
+++ define.php
@@ -0,0 +1,10 @@
+<?php
+/* 定数の定義 */
+define('DEF_INT', 123); // 整数
+define('DEF_FLOAT', 3.14); // 小数
+define('DEF_STRING', "string"); // 文字列
+
+// 定数の仕様
+var_dump(DEF_INT);
+var_dump(DEF_FLOAT);
+var_dump(DEF_STRING);
