4b3fafcb4c0e658d9d4720570045962f0e5ec36d
yusuke <shindo1320366@gmail.com>
2026-04-05 12:41:05 +0000

    演習問題のファイル追加

---
+++ 5-5test.php
@@ -0,0 +1,13 @@
+<?php
+
+$i = 123;
+
+if($i < 50) {
+    echo "50未満 \n";
+} else if($i < 100) {
+    echo "100未満 \n";
+} else if($i < 150) {
+    echo "150未満 \n";
+} else {
+    echo "150以上 \n";
+}
